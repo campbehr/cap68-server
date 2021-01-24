dcef380ecea8964bbb8b4b352ed6d92204c06daf
campbehr <hunter.campbell843@gmail.com>
2021-01-24 14:01:03 -0500

    fixing config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // const dotenv = require("dotenv");
 // dotenv.config();
 module.exports = {
-  post_user: process.env.MAILCHIMP_URL_POST,
-  auth: process.env.MAILCHIMP_AUTH,
-  port: process.env.PORT,
+  post_user: MAILCHIMP_URL_POST,
+  auth: MAILCHIMP_AUTH,
+  port: PORT,
 };
